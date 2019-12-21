68f4857bf608748bb33b3bd52aa12b8836f7fd2b
Chris <chrishaining@yahoo.co.uk>
2019-12-21 15:43:23 +0000

    adds table to baby pandas

--- baby_pandas.py
+++ baby_pandas.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 df1 = pd.DataFrame({
-  'Product ID': [1, 2, 3, 4],
+  'name': ["Alice", "Sandy", "Nev", "Varda"],
+  'age': [32, 21, 104, 60]
 })
 print(df1)
