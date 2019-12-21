025288159320f8a9206b7e2498aa4ff8fe65d2d9
Chris <chrishaining@yahoo.co.uk>
2019-12-21 15:49:50 +0000

    adds second table to babypandas

--- baby_pandas.py
+++ baby_pandas.py
@@ -2,7 +2,17 @@ import pandas as pd
 import numpy as np
 
 df1 = pd.DataFrame({
-  'name': ["Alice", "Sandy", "Nev", "Varda"],
-  'age': [32, 21, 104, 60]
+'name': ["Alice", "Sandy", "Nev", "Varda"],
+'age': [32, 21, 104, 60]
 })
 print(df1)
+
+
+df2 = pd.DataFrame([
+['bike', 'red', 50],
+['car', 'silver', 180],
+['jet', 'black', 30000]
+],
+columns=['vehicle', 'colour', 'speed (mph)']
+)
+print(df2)
