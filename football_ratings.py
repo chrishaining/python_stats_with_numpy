c874bcde2ce4e537b950fb5cd438e6eb9f0c48b6
Chris <chrishaining@yahoo.co.uk>
2019-12-23 20:13:02 +0000

    further tidying of estimate_ability

--- football_ratings.py
+++ football_ratings.py
@@ -29,9 +29,10 @@ reliability = 0.5
 
 def estimate_ability(team, reliability, points, games):
     last_season_ppg = table_2018_2019.loc[table_2018_2019.Team==team, 'PPG']
-    ability = (reliability*(round((points/games), 1))) + ((round((1 - reliability), 1)) * last_season_ppg)
+    # ability = (reliability*(round((points/games), 1))) + ((round((1 - reliability), 1)) * last_season_ppg)
+    raw_ability = (reliability*(points/games)) + ((1 - reliability) * last_season_ppg)
+    ability = round(raw_ability, 1)
     return "Based on a comparison of current form with last season's results, {}'s ability is {}. The measure of ability is points per game".format(team, ability)
-
 
 # expect 2.9
 test1 = estimate_ability('Liverpool', 1.0, 49, 17)
